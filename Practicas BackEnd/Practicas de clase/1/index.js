ed0d81297c14d02fd6f172f020515763910d59a9
Ricardo-Vargas-Gonzalez <ceinnova05251@innovaccion.mx>
2022-12-21 21:39:03 -0600

    first commit

--- Practicas BackEnd/Practicas de clase/1/index.js
+++ Practicas BackEnd/Practicas de clase/1/index.js
@@ -1,1 +1,18 @@
-alert("Hola Mundo")
+parrafoinicial = document.getElementById("parrafoinicial")
+
+/*
+CASES
+
+Pascalcase - Clases primer letra mayuscula es una clase - JAVA
+
+Camelcase - variables, funciones y atributos - JavaScript
+    variable -> parrafoinicial =
+    funcion -> parrafoinicial()
+    atributo -> parrafoinicial
+
+snake_case - Python
+
+CASE (CAPITAL CASE) - Constantes
+
+basicamente es como acomodo mi combinación de mayusculas y minusculas para que se entienda mejor
+*/
